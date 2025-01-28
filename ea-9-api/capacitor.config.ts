1fdfed023d36c947b7d50f7eb506846574e8936d
Alexanderrmagno <22192@portalcedi.edu.mx>
2025-01-28 08:19:20 -0600

    commit 1

---
+++ ea-9-api/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'EA9 - API',
+  webDir: 'www'
+};
+
+export default config;
